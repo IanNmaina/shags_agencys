021c6abf9bc5dda7c1c67d6bd34f23c42c72dd9a
Ian Maina <ianmainan21@gmail.com>
2024-04-26 17:01:40 +0300

    creating a form view

--- properties_for_rent/views.py
+++ properties_for_rent/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .models import Listing
+from .forms import ListingForm
 
 def listing_list(request):
     # Retrieve all listings from the database
@@ -18,4 +19,9 @@ def listing_retrieve(request,pk):
     }
     return render(request,"listing.html",context)
     import requests
-    
+def listing_create(request):
+    form = ListingForm()
+    context = {
+        "form": form
+    }
+    return render(request,"listing_create.html",context)
